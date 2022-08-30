d4c22c88081d7f63add79b08adda21e2475c81e3
Swift Pay Cash <paycashswift@gmail.com>
2022-08-30 06:17:26 +0100

    all

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword3 = 'mongodb://localhost:27017/mern';
+dbPassword = 'mongodb+srv://smartchainearn:HelloToday12@cluster0.1kdamgj.mongodb.net/test'
+
+module.exports = {
+    mongoURI: dbPassword
+};
---
+++ models/User.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  wallet: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  password2:{
+    type: String,
+    required: true
+  },
+  code:{
+    type: String,
+    
+  },
+  availbalance:{
+    type: String,
+    default: '00.00'
+  },
+  withdrawals:{
+    type: String,
+    default: '00.00'
+  },
+  plantype:{
+    type: String,
+    default: 'starter'
+  },
+  refaralsbonus:{
+    type: String,
+    default: '00.00'
+  },
+  referalcode:{
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
